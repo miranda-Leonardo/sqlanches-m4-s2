80a973483bcfdc8848c854a1442d2fc6accb4953
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2022-12-02 11:57:52 -0400

    add: código sql

--- crud.sql
+++ crud.sql
@@ -4,31 +4,85 @@
 -- Criação
 
 -- 1)
-
-
+INSERT INTO clientes 
+	(nome, lealdade)
+VALUES 
+	('Georgia', 0)
+RETURNING *;
 
 -- 2)
-
-
+INSERT INTO pedidos 
+	(status, cliente_id)
+ VALUES 
+ 	('Recebido', 6)
+ RETURNING *;
 
 -- 3)
-
+INSERT INTO produtos_pedidos 
+	(pedido_id, produto_id)
+VALUES 
+	(6, 1),
+	(6, 2),
+	(6, 6),
+	(6, 8),
+	(6, 8)
+RETURNING *;
 
 
 -- Leitura
 
 -- 1)
-
+SELECT 
+	c.id,
+	c.nome,
+	c.lealdade,
+	p.id,
+	p.status,
+	p.cliente_id,
+	prod.id,
+	prod.nome,
+	prod.tipo,
+	round(prod.preco:: NUMERIC, 2) preco,
+	prod.pts_de_lealdade 
+FROM 
+	clientes c
+JOIN
+	pedidos p ON p.cliente_id = c.id 
+JOIN
+	produtos_pedidos pp ON pp.pedido_id = p.id
+JOIN 
+	produtos prod ON prod.id = pp.produto_id
+WHERE
+	lower(c.nome) = 'georgia';
 
 
 -- Atualização
 
 -- 1)
-
+UPDATE 
+	clientes 
+SET
+	lealdade = (
+		SELECT 
+			sum(prod.pts_de_lealdade) 
+		FROM 
+			pedidos p 
+		JOIN
+			produtos_pedidos pp ON pp.pedido_id  = p.id 
+		JOIN 
+			produtos prod ON prod.id = pp.produto_id
+		WHERE 
+			p.cliente_id = 6
+	)
+WHERE
+	lower(clientes.nome) = 'georgia'
+RETURNING *;
 
 
 -- Deleção
 
 -- 1)
-
+DELETE FROM clientes c 
+WHERE lower(c.nome) = 'marcelo'
+RETURNING *;
 
--- selects.sql
+++ selects.sql
@@ -2,11 +2,69 @@
 -- Seleções de dados
 
 -- 1)
+SELECT 
+	ped.id ,
+	ped.status,
+	ped.cliente_id,
+	prod.id,
+	prod.nome,
+	prod.tipo,
+	ROUND(prod.preco::NUMERIC, 2) preco,
+	prod.pts_de_lealdade pontos_de_lealdade_do_produto
+FROM 
+	pedidos ped
+JOIN 
+	produtos_pedidos pp ON pp.pedido_id = ped.id
+JOIN 
+	produtos prod ON prod.id = pp.produto_id;
 
 -- 2)
+SELECT 
+	ped.id
+FROM 
+	pedidos ped
+JOIN
+	produtos_pedidos pp ON pp.produto_id = (
+		SELECT p.id FROM produtos p WHERE lower(p.nome) = 'fritas' 
+	) AND ped.id = pp.pedido_id ;
 
 -- 3)
+SELECT 
+	c.nome gostam_de_fritas
+FROM 
+	clientes c
+JOIN
+	pedidos ped ON ped.cliente_id = c.id 
+JOIN 
+	produtos_pedidos pp ON pp.pedido_id = ped.id
+JOIN
+	produtos prod ON prod.id = pp.produto_id 
+WHERE 
+	lower(prod.nome) = 'fritas' ;
 
 -- 4)
+SELECT 
+	round(sum(prod.preco)::NUMERIC, 2) sum
+FROM 
+	produtos prod
+JOIN 
+	produtos_pedidos pp ON pp.produto_id = prod.id 
+JOIN 
+	pedidos ped ON ped.id = pp.pedido_id
+JOIN 
+	clientes c ON c.id = ped.cliente_id
+WHERE
+	lower(c.nome) = 'laura' ;
 
 -- 5)
+SELECT
+	prod.nome,
+	count(prod.nome) 
+FROM 
+	produtos prod
+JOIN
+	produtos_pedidos ped ON ped.produto_id = prod.id
+GROUP BY 
+	prod.nome
+ORDER BY 
+	prod.nome;
